038da0efff69c28526346aa7f235c668ea9f2102
Naqib Hishamuddin <naqibhishamuddin@gmail.com>
2023-04-07 23:35:30 +0800

    refactor(mocks): update msw implementation to compatible with vitest

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,11 @@
 import { rest } from "msw";
 
+const restApi = (path: string) => {
+  return `https://api.test.com/${path}`;
+};
+
 export const handlers = [
-  rest.get("/todo", (_, res, ctx) => {
+  rest.get(restApi("todo"), (_, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.json({
--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,2 +1,13 @@
-export * from "./browser";
-export * from "./handlers";
+async function initMocks() {
+  if (typeof window === "undefined") {
+    const { server } = await import("./server");
+    server.listen();
+  } else {
+    const { worker } = await import("./browser");
+    worker.start();
+  }
+}
+
+initMocks();
+
+export {};

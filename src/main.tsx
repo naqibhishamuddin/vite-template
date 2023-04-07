11d6edc7743f2c4bfb5a0472bdaadf22090b3d68
Naqib Hishamuddin <naqibhishamuddin@gmail.com>
2023-04-07 23:36:06 +0800

    refactor(main.tsx): update mock implementation

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { worker } from "mocks";
 import App from "./App";
 import "./index.css";
 
 if (process.env.NODE_ENV === "development") {
-  worker.start();
+  require("../mocks");
 }
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(

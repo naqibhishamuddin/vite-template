40e6346e467a6086ceac7ff86ed5c8509089be84
Naqib Hishamuddin <naqibhishamuddin@gmail.com>
2023-04-07 23:38:38 +0800

    test(__tests__): add test suite for api mock

--- src/__tests__/sample.test.tsx
+++ src/__tests__/sample.test.tsx
@@ -10,4 +10,10 @@ describe("Sample test suite", () => {
     render(<p>Hello world</p>);
     expect(screen.getByText("Hello world")).toBeInTheDocument();
   });
+
+  it("should be able to mock the API automatically", async () => {
+    const response = await fetch("https://api.test.com/todo");
+    const data = await response.json();
+    expect(data.tasks).toStrictEqual(["Clean the house", "Walk the dog"]);
+  });
 });
